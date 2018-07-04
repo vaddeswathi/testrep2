ff710482c6ea159b7abf876e240c267d4672ae84
vaddeswathi <swathirose91@gmail.com>
2018-07-04 15:59:44 +0200

    created object to new class

--- Second_Project2J_Old/src/com/hdfc/loans/businessloans/NewClass.java
+++ Second_Project2J_Old/src/com/hdfc/loans/businessloans/NewClass.java
@@ -6,13 +6,15 @@ public class NewClass {
 		int b=2;
 		int c;
 		c=a+b;
+		
 		System.out.println("add of a and b is:"+c);
 		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is the new class added");
-
+		NewClass obj = new NewClass();
+		obj.n1();
 	}
 
 }
