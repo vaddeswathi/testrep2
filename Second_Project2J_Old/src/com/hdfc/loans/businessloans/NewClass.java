693d25abefbc293e8342a99985574280307acd9b
vaddeswathi <swathirose91@gmail.com>
2018-07-04 15:44:15 +0200

    added a new class to package

---
+++ Second_Project2J_Old/src/com/hdfc/loans/businessloans/NewClass.java
@@ -0,0 +1,11 @@
+package com.hdfc.loans.businessloans;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is the new class added");
+
+	}
+
+}
