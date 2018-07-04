5c5dde23c6ed65e26ac5eb5358bb9c321ef42677
vaddeswathi <swathirose91@gmail.com>
2018-07-04 15:50:12 +0200

    added a n1 method to new class

--- Second_Project2J_Old/src/com/hdfc/loans/businessloans/NewClass.java
+++ Second_Project2J_Old/src/com/hdfc/loans/businessloans/NewClass.java
@@ -1,7 +1,14 @@
 package com.hdfc.loans.businessloans;
 
 public class NewClass {
-
+	public void n1() {
+		int a=1;
+		int b=2;
+		int c;
+		c=a+b;
+		System.out.println("add of a and b is:"+c);
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is the new class added");
